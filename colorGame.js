cadbef9e64b4c34a2090cacf62f5dcc5f0652aa6
Ayush Malviya <ayushmalviya27@gmail.com>
2017-12-18 09:32:09 +0530

    Hardcoded Solution

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,10 @@
 var colors = [
-	"rgb(255,0,0)",
-	"rgb(255,0,255)",
-	"rgb(0,255,255)",
-	"rgb(0,255,0)",
-	"rgb(0,0,255)",
-	"rgb(255,255,0)"
+	"rgb(255, 0, 0)",
+	"rgb(255, 0, 255)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 255, 0)"
 ]
 
 var squares=document.querySelectorAll(".square");
@@ -14,5 +14,20 @@ var colorDisplay=document.getElementById('colorDisplay');
 colorDisplay.textContent=pickedColor.toUpperCase();
 
 for (var i = 0; i < squares.length; i++) {
+	//add initial colors to square
 	squares[i].style.background=colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click",function() {
+	//grab color of clicked square
+	var clickedColor=this.style.background;
+	//compare color to clicked color
+	// if(clickedColor === pickedColor){
+	// 	alert("Correct");
+	// }	else{
+	// 		alert("Wrong");
+	// 	}
+	console.log(clickedColor); //gives none repeat scroll 0% 0%
+	console.log(pickedColor);
+	});
 }
